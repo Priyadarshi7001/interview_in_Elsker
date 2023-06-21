12da7d296549d452e80f2dabbe010c03e1da6bc7
Priyadarshi7001 <110182872+Priyadarshi7001@users.noreply.github.com>
2023-06-21 17:31:45 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,152 @@
+import React, { useState, useRef, useEffect } from "react";
+import "./App.css";
+
+const App = () => {
+  const photoInputref = useRef();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [photo, setPhoto] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [nameIsValid, setNameIsValid] = useState(false);
+  const [emailIsValid, setemailIsValid] = useState(false);
+  const [photoIsValid, setphotoIsValid] = useState(false);
+  const [touchedName, setTouchedName] = useState(false);
+  const [touchedemail, setTouchedemail] = useState(false);
+  const [touchedPhoto, setTouchedPhoto] = useState(false);
+
+
+  useEffect(()=> {
+    if (nameIsValid){
+      setTouchedName(true);
+      return;
+    }
+    if (emailIsValid){
+      setTouchedemail(true);
+      return;
+    }
+    if(photoIsValid){
+      setTouchedPhoto(true);
+      return;
+    }
+  },[nameIsValid, emailIsValid, photoIsValid]);
+
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePhotoChange = (event) => {
+    setPhoto(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (name.trim() === "") {
+      setErrorMessage("Please fill in all fields.");
+      setNameIsValid(false);
+    }
+
+    else if (email.trim() === "") {
+      setErrorMessage("Please fill in all fields.");
+      setemailIsValid(false);
+    }
+    else if (!photo){
+      setErrorMessage("Please fill in all fields.");
+      setphotoIsValid(false);
+    }
+     else {
+      setNameIsValid(true);
+      setemailIsValid(true);
+      setphotoIsValid(true);
+
+      console.log("Name:", name);
+      console.log("Email:", email);
+      console.log("Photo:", photo);
+      photoInputref.current.value = '';
+      clearForm();
+    }
+  };
+
+  const clearForm = () => {
+    setName("");
+    setEmail("");
+    setErrorMessage("");
+    setPhoto("");
+  };
+
+  
+  const validName = !nameIsValid && touchedName;
+  const validEmail = !emailIsValid && touchedemail;
+  const validPhoto = !photoIsValid && touchedPhoto;
+
+// const errorInputNameClasses = nameIsValid ? "" : "input-error";
+// const errorInputMailClasses = emailIsValid ? "" :  "input-error";
+// const errorInputPhotoClasses = photoIsValid ? "" :  "input-error";
+
+const errorInputNameClasses = validName ? "input-error" : "";
+const errorInputMailClasses = validEmail ? "input-error" : "";
+const errorInputPhotoClasses = validPhoto ? "input-error" : "";
+
+
+
+ 
+
+  return (
+    <div className="user-form">
+      <div className="heading">
+      <h1>Input Form</h1>
+      </div>
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+      <form onSubmit={handleSubmit}>
+        <ul type="none">
+          <li key={1}>
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={handleNameChange}
+            className={errorInputNameClasses}
+          />
+        </label>
+        </li>
+        
+        <li key={2}>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+            className={errorInputMailClasses}
+          />
+        </label>
+        </li>
+       
+        <li key={3}>
+        <label>
+          Photo:
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handlePhotoChange}
+            ref={photoInputref}
+            className={errorInputPhotoClasses}
+          />
+        </label>
+        </li>
+        
+        <li key={4}>
+        <button type="submit" className="submit-button">Submit</button>
+        </li>
+        </ul>
+      </form>
+    </div>
+  );
+};
+
+export default App;
